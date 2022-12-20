1786a22bc5ef966aafe1f2280c486611b432077f
KanzaTahreem <tehreemmsk411@gmail.com>
2022-12-21 02:35:26 +0500

    Add remover method in Animal class

--- animal.rb
+++ animal.rb
@@ -1,3 +1,5 @@
+require_relative './remover'
+
 class Animal
   def initialize(type, number_of_legs, name = 'Unknown')
     @id = Random.rand(1..1000)
@@ -11,5 +13,10 @@ class Animal
 
   def speak
     'grrrr'
+  end
+
+  def remove_leg
+    remover = Remover.new()
+    @number_of_legs = remover.decrease(@number_of_legs)
   end
 end
