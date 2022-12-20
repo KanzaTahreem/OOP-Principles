bcd8e24b5f78ea4b9e742c1631d8f8d5186e87fd
KanzaTahreem <tehreemmsk411@gmail.com>
2022-12-21 02:06:07 +0500

    Try out inheritance

--- test.rb
+++ test.rb
@@ -1,4 +1,6 @@
 require_relative './animal'
+require './dog'
+require './spider'
 
 first_animal = Animal.new('dog', 4, 'Rex')
 first_animal.id
@@ -16,3 +18,9 @@ third_animal = Animal.new('spider', 8, 'Wilma')
 puts first_animal.speak # Woof, woof
 puts second_animal.speak
 puts third_animal.speak # ...
+
+dog = Dog.new('black', 'Rex')
+spider = Spider.new(85, 'Wilma')
+
+puts dog.bring_a_stick # Here is your stick: ---------
+puts spider.make_a_web # www
