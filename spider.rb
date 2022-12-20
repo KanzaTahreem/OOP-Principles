82478338d960b0175bf37557166c9f002d9ccc59
KanzaTahreem <tehreemmsk411@gmail.com>
2022-12-21 02:06:50 +0500

    Fix linter errors

--- spider.rb
+++ spider.rb
@@ -1,11 +1,11 @@
 require './animal'
 
 class Spider < Animal
-  def initialize(web_strength_level, name = "Unknown")
-    super("spider", 8, name)
+  def initialize(web_strength_level, name = 'Unknown')
+    super('spider', 8, name)
     @web_strength_level = web_strength_level
   end
-  
+
   def make_a_web
     'www'
   end
