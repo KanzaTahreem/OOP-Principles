a15c4063ae053e5af14e78a1e1ed30960f32348c
KanzaTahreem <tehreemmsk411@gmail.com>
2022-12-21 02:20:15 +0500

    Try out all principles

--- test.rb
+++ test.rb
@@ -3,24 +3,24 @@ require './dog'
 require './spider'
 
 first_animal = Animal.new('dog', 4, 'Rex')
-first_animal.id
-first_animal.type
-first_animal.name
-first_animal.number_of_legs
-
 second_animal = Animal.new('cat', 8)
-second_animal.name
-second_animal.name = 'Fluffy'
-second_animal.name
 
-third_animal = Animal.new('spider', 8, 'Wilma')
+puts first_animal.id # randomly generated number
+puts first_animal.type # dog
+puts first_animal.name # Rex
+puts first_animal.number_of_legs # 4
 
-puts first_animal.speak # Woof, woof
-puts second_animal.speak
-puts third_animal.speak # ...
+puts second_animal.name # Unknown
+puts second_animal.name = 'Fluffy' # Fluffy
+puts second_animal.name # Fluffy
 
+animal = Animal.new('lion', 4, 'Rex')
 dog = Dog.new('black', 'Rex')
 spider = Spider.new(85, 'Wilma')
 
 puts dog.bring_a_stick # Here is your stick: ---------
 puts spider.make_a_web # www
+
+puts animal.speak # grrrr
+puts dog.speak # Woof, woof
+puts spider.speak # ...
