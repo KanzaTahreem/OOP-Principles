3a0ae864038c527abb5f3df9910c112fbf8c61a3
KanzaTahreem <tehreemmsk411@gmail.com>
2022-12-21 02:46:09 +0500

    Modify Animal class to add method and instance variable

--- animal.rb
+++ animal.rb
@@ -6,6 +6,7 @@ class Animal
     @name = name
     @number_of_legs = number_of_legs
     @type = type
+    liked_food = NoFood.new()
   end
 
   attr_reader :id, :type, :number_of_legs
@@ -18,5 +19,9 @@ class Animal
   def remove_leg
     remover = Remover.new()
     @number_of_legs = remover.decrease(@number_of_legs)
+  end
+
+  def likes_food?(food)
+    @liked_food.is_liked?(food)
   end
 end
